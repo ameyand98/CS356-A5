7c41dcda0bf4925f5acd7b59cebed183a6ae375c
Ameya <ameyand.98@gmail.com>
2022-11-22 02:52:39 -0600

    incomplete argument parsing

--- src/edu/ut/cs/sdn/simpledns/SimpleDNS.java
+++ src/edu/ut/cs/sdn/simpledns/SimpleDNS.java
@@ -1,11 +1,73 @@
 package edu.ut.cs.sdn.simpledns;
+import java.net.*;
+import java.io.IOException;
+import java.io.*;
+import java.util.*;
+import java.nio.ByteBuffer;
 
 public class SimpleDNS 
 {
+
+	private ArrayList<CIDRData> csvList;
+
 	public static void main(String[] args)
 	{
         System.out.println("Hello, DNS!"); 
 
+		if (!validArguments(args)) {
+			System.out.println("Invalid arguments!");
+			return;
+		}
+
+		InetAddress rootDNSip;
+		String[] csvAndRootIp = parseArgs(args);
+
+		try {
+			rootDNSip = InetAddress.getByName(csvAndRootIp[0]);
+		} catch (UnknownHostException e) {
+			System.out.println("Invalid root IP");
+			return;
+		}
+
+
 		
+	}
+
+	private static boolean validArguments(String[] args) {
+		return (args.length == 4 &&
+            	((args[0].equals("-r") && args[2].equals("-e")) ||
+                (args[2].equals("-r") && args[0].equals("-e"))));
+	}
+
+	// First element is root ip string 
+	// Second element is csv file
+	private static String[] parseArgs(Strin[] args) {
+		String[] argVals = new String[2];
+		argVals[0] = args[0].equals("-r") ? args[1] : args[3];
+		argVals[1] = args[0].equals("-e") ? args[1] : args[3];
+	}
+
+	private parseCSV(String csvFile) {
+		csvList = new ArrayList<>();
+		FileReader file = new FileReader(csvFile);
+		BufferedReader reader = new BufferedReader(file);
+
+		String currLine;
+		while ((currLine == reader.readLine()) != null) {
+			currLine = currLine.trim();
+
+		}
+	}
+
+	private parseCSVLineToCIDRData(String record) throws UnknownHostException {
+		String[] CIDRData = record.split("[/,]");
+
+		InetAddress netAddrObj = InetAddress.getByName(CIDRData[0]);
+		ByteBuffer buff = ByteBuffer.wrap(netAddrObj.getAddress());
+		int networkAddr = buff.getInt();
+		int subnetMask = (0xffffffff) << (Integer.SIZE - Integer.parseInt(CIDRData[1]));
+
+		
+
 	}
 }
