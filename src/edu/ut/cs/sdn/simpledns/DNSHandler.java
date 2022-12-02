437f6a0f6fa7383d991879382614c782fc995213
sjp2993 <sjp2993@node-0.sjp2993-ameyad98.ut-cs356-f22-pg0.utah.cloudlab.us>
2022-12-02 02:11:34 -0700

    Recursive Resolve Implemented

--- src/edu/ut/cs/sdn/simpledns/DNSHandler.java
+++ src/edu/ut/cs/sdn/simpledns/DNSHandler.java
@@ -51,7 +51,7 @@ public class DNSHandler {
         } else {
             //non-recursively resolve
             System.out.println("Non-recursively Resolved");
-            replyPacket = nonRecursivelyResolve(dnsServerIP, dnsPacket, request);
+            replyPacket = nonRecursivelyResolve(dnsServerIP, dnsPacket);
         }
 
         DNS reply = DNS.deserialize(replyPacket.getData(), replyPacket.getLength());
@@ -63,13 +63,7 @@ public class DNSHandler {
         clientConnection.sendDNSPacket(replyPacket);
     }
 
-    public DatagramPacket recursivelyResolve(InetAddress rootAddr, DatagramPacket query, DNS req) throws Exception {
-        boolean resolved = false;
-        byte[] bytes = new byte[4096];
-        return new DatagramPacket(bytes, bytes.length);
-    }
-
-    public DatagramPacket nonRecursivelyResolve(InetAddress rootAddr, DatagramPacket query, DNS req) throws Exception {
+    public DatagramPacket nonRecursivelyResolve(InetAddress rootAddr, DatagramPacket query) throws Exception {
         DatagramPacket recvPacket;
         DatagramPacket newQuery = new DatagramPacket(query.getData(), query.getLength(), rootAddr, DNS_SEND_PORT);
 
@@ -78,6 +72,113 @@ public class DNSHandler {
 
         return recvPacket;
     }
+
+    // if answer found
+    //  if CNAME -> recurse to resolve
+    //  else answer found (actually resolve)
+    // else answer not found
+    //  NS Addr found
+    //      Return Packet with new NS addr
+    //  NS Addr not found
+    //      Return Same Packet (after setting lists)
+    public DatagramPacket recursivelyResolve(InetAddress rootAddr, DatagramPacket query, DNS req) throws Exception {
+        List<DNSResourceRecord> tgtCNAMEs  = new ArrayList<>();
+        List<DNSResourceRecord> tgtAuths  = new ArrayList<>();
+        List<DNSResourceRecord> tgtAdds = new ArrayList<>();
+        
+        boolean isResolved = false;
+        DatagramPacket recvPacket = null;
+        DatagramPacket newQuery = new DatagramPacket(query.getData(), query.getLength(), rootAddr, DNS_SEND_PORT);
+
+        clientConnection.sendDNSPacket(newQuery);
+        while(!isResolved) {
+
+            recvPacket = clientConnection.receiveDNSPacket();
+            DNS recv = DNS.deserialize(recvPacket.getData(), recvPacket.getData().length);
+            System.out.println("RECEIVED PACKET DESERIALIZED IS: " + recv.toString());
+
+            List<DNSResourceRecord> srcAuths = recv.getAuthorities();
+            List<DNSResourceRecord> srcAdds = recv.getAdditional();
+
+            
+            boolean validTypes = false;
+            int authIndex = 0;
+            while (!validTypes && authIndex < srcAuths.size()) {
+                short curType = srcAuths.get(authIndex++).getType();
+                validTypes = (curType == DNS.TYPE_A || curType == DNS.TYPE_AAAA || curType == DNS.TYPE_CNAME || curType == DNS.TYPE_NS);
+            }
+            System.out.println("Authority/Additional has data: " + validTypes + " and additionals are of size " + srcAdds.size() + " and authorities are of size " + srcAuths.size());
+            tgtAuths = validTypes ? srcAuths: tgtAuths;
+            tgtAdds = srcAdds.size() > 0 ? srcAdds: tgtAdds;
+
+            List<DNSResourceRecord> curAnswers = recv.getAnswers();
+            if(curAnswers.size() > 0) {
+                //answer found
+                DNSResourceRecord ans = curAnswers.get(0);
+                if (ans.getType() == DNS.TYPE_CNAME) {
+                    System.out.println("CNAME -> resolve through authority section");
+                    // CNAME -> resolve through authority section
+                    tgtCNAMEs.add(ans);
+
+                    DNS newQueryHeader = buildDNSQueryHeader(req, recv, ans);
+
+                    newQuery = new DatagramPacket(newQueryHeader.serialize(), newQueryHeader.getLength(), rootAddr, DNS_SEND_PORT);
+                    clientConnection.sendDNSPacket(newQuery);
+                } else {
+                    // (BASE CASE) resolved -> build recvPacket
+                    System.out.println("RESOLVED -> build recvPacket");
+                    isResolved = true;
+
+                    List<DNSResourceRecord> replyAnswers = buildAnswers(curAnswers, tgtCNAMEs, req);
+
+                    if(srcAuths.size() == 0) {
+                        System.out.println("RESET authorities");
+                        recv.setAuthorities(tgtAuths);
+                    }
+
+                    if(srcAdds.size() == 0) {
+                        System.out.println("RESET additionals");
+                        recv.setAdditional(tgtAdds);
+                    }
+
+                    recv.setAnswers(replyAnswers);
+                    recv.setQuestions(req.getQuestions());
+
+                    setFlags(recv, false);
+
+                    recvPacket = new DatagramPacket(recv.serialize(), recv.getLength());
+                }
+            } else {
+                //answer not found
+                InetAddress tgtNSAddr = getTgtNSAddr(srcAuths, srcAdds);
+                if(tgtNSAddr == null) {
+                    System.out.println("NOT RESOLVED JUST EARLY END AS NOTHING FOUND ");
+                    //next NS Address not found in additional/authority section -> ignore and just setup reply packet(?)
+                    DNS curRecv = new DNS();
+                    List<DNSResourceRecord> replyAnswers = buildAnswers(curAnswers, tgtCNAMEs, req);
+                    
+                    curRecv.setQuestions(recv.getQuestions());
+                    curRecv.setAnswers(replyAnswers);
+                    curRecv.setAuthorities(getEntryList(recv.getAuthorities(), tgtAuths));
+                    curRecv.setAdditional(getEntryList(recv.getAdditional(), tgtAdds));
+
+                    setFlags(curRecv, false);
+
+                    curRecv.setId(req.getId());
+
+                    return new DatagramPacket(curRecv.serialize(), curRecv.getLength());
+                } else {
+                    newQuery = new DatagramPacket(newQuery.getData(), newQuery.getLength(), tgtNSAddr, DNS_SEND_PORT);
+                    clientConnection.sendDNSPacket(newQuery);
+                } 
+            }
+
+        }
+
+        assert(recvPacket != null);
+        return recvPacket;
+    }
+    
 
     public boolean isRequestValid(DNS request) {
         boolean validQuestions = true;
@@ -97,5 +198,91 @@ public class DNSHandler {
         }
         return (validQuestions && request.getOpcode() == 0 && request.isQuery());
     }
+
+    private InetAddress getTgtNSAddr(List<DNSResourceRecord> srcAuths, List<DNSResourceRecord> srcAdds) {
+        for(DNSResourceRecord authEntry: srcAuths) {
+            String nsNameStr = ((DNSRdataName) authEntry.getData()).getName();
+            for(DNSResourceRecord addEntry: srcAdds) {
+                if(authEntry.getType() == DNS.TYPE_NS && addEntry.getType() == DNS.TYPE_A && addEntry.getName().equals(nsNameStr)) {
+                    System.out.println("TGT NS FOUND: " + nsNameStr);
+                    return ((DNSRdataAddress) addEntry.getData()).getAddress();
+                }
+            }
+        }
+        return null;
+    }
+
+    private List<DNSResourceRecord> getEntryList(List<DNSResourceRecord> curEntries, List<DNSResourceRecord> tgtEntries) {
+        List<DNSResourceRecord> res = new ArrayList<>();
+        //Only include A, AAAA, NS, and CNAME entries
+        for(DNSResourceRecord entry: curEntries) {
+            short curType = entry.getType();
+            if (curType == DNS.TYPE_A || curType == DNS.TYPE_AAAA || curType == DNS.TYPE_CNAME) {
+                res.add(entry);
+            }
+        }
+        return res.size() == 0 ? tgtEntries : res;
+    }
+
+    private List<DNSResourceRecord> buildAnswers(List<DNSResourceRecord> answers, List<DNSResourceRecord> resolvedCNAMEs, DNS req) {
+        //HANDLE TXT Records
+        if (req.getQuestions().get(0).getType() == DNS.TYPE_A) {
+            //TODO: Handle adding TXT records by going through CSV
+        }
+
+        for(DNSResourceRecord resolvedCNAME: resolvedCNAMEs) {
+            answers.add(0, resolvedCNAME);
+        }
+
+
+        return answers;
+    }
+
+    private DNS buildDNSQueryHeader(DNS req, DNS recv, DNSResourceRecord ans) {
+        DNS queryHeader = new DNS();
+
+        DNSQuestion question = new DNSQuestion();
+        //https://stackoverflow.com/questions/7875513/java-cast-interface-to-class
+        DNSRdataName data = (DNSRdataName)ans.getData(); 
+        question.setName(data.getName());
+        question.setType(recv.getQuestions().get(0).getType());
+
+        queryHeader.setId(req.getId());
+        setFlags(queryHeader, true);
+
+        List<DNSQuestion> questions = new ArrayList<>();
+        questions.add(question);
+        queryHeader.setQuestions(questions);
+
+        return queryHeader;
+    }
+
+    private void setFlags(DNS header, boolean isQuery) {
+        header.setQuery(isQuery);
+        header.setOpcode(DNS.OPCODE_STANDARD_QUERY);
+        header.setTruncated(false);
+        header.setRecursionDesired(true);
+        header.setAuthenicated(false);
+
+        if (!isQuery) {
+            header.setAuthoritative(false);
+            header.setRecursionAvailable(true);
+            header.setCheckingDisabled(false);
+            header.setRcode(DNS.RCODE_NO_ERROR);
+        }
+    }
+
+    private void setTgtLists(List<DNSResourceRecord> srcAuths, List<DNSResourceRecord> srcAdds, List<DNSResourceRecord> tgtAuths, List<DNSResourceRecord> tgtAdds) {
+        boolean validTypes = false;
+        int authIndex = 0;
+        while (!validTypes && authIndex < srcAuths.size()) {
+            short curType = srcAuths.get(authIndex++).getType();
+            validTypes = (curType == DNS.TYPE_A || curType == DNS.TYPE_AAAA || curType == DNS.TYPE_CNAME || curType == DNS.TYPE_NS);
+        }
+        System.out.println("Authority/Additional has data: " + validTypes + " and additionals are of size " + srcAdds.size() + " and authorities are of size " + srcAuths.size());
+        tgtAuths = validTypes ? srcAuths: tgtAuths;
+        tgtAdds = srcAdds.size() > 0 ? srcAdds: tgtAdds;
+    }
+
 
 }
